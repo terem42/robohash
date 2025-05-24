611c43e378f7185d4b1221cf2694b5f73060e16f
Andrey Prokopenko <job@terem.at>
2025-05-24 20:04:44 +0200

    update

--- create_project_sources_text_file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find . -type f \( -name "*.go" -o -name "go.*" -o -name "Dockerfile" -o -name "*.sh" \) -print0 | xargs -0 -I{} sh -c 'cat "{}"; echo "---------------------"' > project_sources.txt
-echo "--------------------------" >> ./project_sources.txt
-tree -d ./assets >> ./project_sources.txt
-
